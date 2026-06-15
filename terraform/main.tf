817a315f322c047805fce303828d8847dbd062b8
kathleendrv <kathleen.drv@gmail.com>
2026-06-15 19:47:42 -0400

    notification-lambda

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,7 +32,7 @@ resource "aws_lambda_function" "api_lambda" {
   role             = aws_iam_role.lambda_role.arn
   handler          = "bootstrap"        # Obligatorio para lambdas de Go en formato al2023
   runtime          = "provided.al2023"  # Entorno Linux optimizado de AWS
-  timeout          = 15
+  timeout          = 30
 
   environment {
     variables = {
